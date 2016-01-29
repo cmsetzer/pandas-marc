030e8b9740274f1d45e4e3de3ea8bf9d6c63cdcc
Christopher Setzer <csetzer.public@gmail.com>
2016-01-29 09:31:08 -0700

    Add tests for alternate delimiters

--- tests/test_pandas_marc.py
+++ tests/test_pandas_marc.py
@@ -21,3 +21,21 @@ def test_marcdataframe_produces_correct_marc_records(dataframe, records):
     output = [record.as_marc() for record in mdf.records]
     expected = [record.as_marc() for record in records]
     assert output == expected
+
+
+def test_marcdataframe_with_other_occurrence_delimiter(dataframe, records):
+    # Find and replace backslashes in original dataframe with pipes
+    dataframe = dataframe.replace(r'\\', '|', regex=True)
+    mdf = MARCDataFrame(dataframe, occurrence_delimiter='|')
+    output = [record.as_marc() for record in mdf.records]
+    expected = [record.as_marc() for record in records]
+    assert output == expected
+
+
+def test_marcdataframe_with_other_subfield_delimiter(dataframe, records):
+    # Find and replace double daggers in original dataframe with dollar signs
+    dataframe = dataframe.replace(r'\$', '‡', regex=True)
+    mdf = MARCDataFrame(dataframe, subfield_delimiter='‡')
+    output = [record.as_marc() for record in mdf.records]
+    expected = [record.as_marc() for record in records]
+    assert output == expected
