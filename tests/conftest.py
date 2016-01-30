b4cb5ae04ed2723194e3f3c668357e6c48c24d75
Christopher Setzer <csetzer.public@gmail.com>
2016-01-30 12:02:40 -0700

    Allow tests to be executed from anywhere
    This commit fixture-izes the tests directory path into an absolute path so that tests can be run from anywhere, not just the project home.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,28 @@
 """Configuration and fixtures for pandas-marc tests."""
 
+import os
+
 import pandas as pd
 from pymarc import MARCReader
 import pytest
 
 
 @pytest.fixture(scope='session')
-def dataframe():
-    dataframe = pd.read_csv('tests/fixtures/dataframe.csv', dtype=str)
+def tests_path():
+    return os.path.dirname(os.path.realpath(__file__))
+
+
+@pytest.fixture(scope='session')
+def dataframe(tests_path):
+    dataframe_path = os.path.join(tests_path, 'fixtures/dataframe.csv')
+    dataframe = pd.read_csv(dataframe_path, dtype=str)
     dataframe = dataframe.fillna('')
     return dataframe
 
 
 @pytest.fixture(scope='session')
-def records():
-    with open('tests/fixtures/records.mrc', 'rb') as records_file:
+def records(tests_path):
+    records_path = os.path.join(tests_path, 'fixtures/records.mrc')
+    with open(records_path, 'rb') as records_file:
         reader = MARCReader(records_file)
         return list(reader)
